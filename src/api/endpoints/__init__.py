b3fe40ec26a786e9d168d34a92a899809bff6b23
Katrin <normik2605@gmail.com>
2026-06-22 21:36:03 +0000

    Добавлены эндпоинты time_slots для кафе

--- src/api/endpoints/__init__.py
+++ src/api/endpoints/__init__.py
@@ -1,1 +1,2 @@
 from .dish import router as dish_router  # noqa
+from .slot import router as slot_router  # noqa
--- src/api/routers.py
+++ src/api/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from src.api.endpoints import (
     dish_router,
+    slot_router,
 )
 
 main_router = APIRouter(prefix='/api/vi')
@@ -9,4 +10,10 @@ main_router.include_router(
     dish_router,
     prefix='/dishes',
     tags=['Dishes'],
+)
+
+main_router.include_router(
+    slot_router,
+    prefix='/cafes/{cafe_id}/time_slots',
+    tags=['TimeSlots'],
 )

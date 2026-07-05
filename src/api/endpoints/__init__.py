759ba900d71663936d917ee1e2e36c4b5b29686d
Arthur Fissunov <artur-fisunov@mail.ru>
2026-07-06 01:41:13 +0300

    Добавлен healthcheck для мониторинга состояния сервиса

--- src/api/endpoints/__init__.py
+++ src/api/endpoints/__init__.py
@@ -2,6 +2,7 @@ from src.api.endpoints.actions import router as action_router
 from src.api.endpoints.auth import router as auth_router
 from src.api.endpoints.cafe import router as cafe_router
 from src.api.endpoints.dish import router as dish_router
+from src.api.endpoints.health import router as health_router
 from src.api.endpoints.media import router as media_router
 from src.api.endpoints.slot import router as slot_router
 from src.api.endpoints.table import router as table_router
@@ -12,6 +13,7 @@ __all__ = [
     'auth_router',
     'cafe_router',
     'dish_router',
+    'health_router',
     'media_router',
     'slot_router',
     'table_router',
--- src/api/routers.py
+++ src/api/routers.py
@@ -5,6 +5,7 @@ from src.api.endpoints import (
     auth_router,
     cafe_router,
     dish_router,
+    health_router,
     media_router,
     slot_router,
     table_router,
@@ -12,6 +13,9 @@ from src.api.endpoints import (
 )
 
 main_router = APIRouter(prefix='/api/v1')
+
+main_router.include_router(health_router)
+
 main_router.include_router(
     dish_router,
     prefix='/dishes',
